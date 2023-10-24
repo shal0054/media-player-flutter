77d64b94ffdaf127482549607eebaf4e9e8714b7
Karim Shaloh <kshaloh@gmail.com>
2023-10-24 03:05:26 -0400

    add track objects

--- js/index.js
+++ js/index.js
@@ -1,25 +1,68 @@
-const SONGS = [
-	//list of song objects
+const TRACKS = [
+	{
+		id: 0,
+		artist: '2pac ft. Amr Diab',
+		album: 'Remix',
+		track: "Baby Don't Cry",
+		length: '4:12',
+		path: '/media/songs/2Pac-ft-Amr-Diab-Baby-Dont-Cry-Arabic.mp3',
+		img: '/media/img/2pac.jpg',
+	},
+	{
+		id: 1,
+		artist: 'Elissa',
+		album: 'Remix',
+		track: 'Ahla Donia',
+		length: '3:58',
+		path: 'media/songs/2pac-Elissa-Arabic-Remix-Ahla-Donia.mp3',
+		img: 'media/img/Alleyezonme.jpeg',
+	},
+	{
+		id: 2,
+		artist: 'Outlandish',
+		album: 'Single',
+		track: 'Aicha',
+		length: '4:39',
+		path: '/media/songs/Aisha-Outlandish.mp3',
+		img: '/media/img/outlandish.jpg',
+	},
+	{
+		id: 3,
+		artist: 'Phil Collins',
+		album: 'Instrumental Mix',
+		track: 'In The Air Tonight',
+		length: '3:20',
+		path: '/media/songs/In-The-Air-Tonight-Instrumental-Mix.mp3',
+		img: '/media/img/phil.jpg',
+	},
+	{
+		id: 4,
+		artist: 'Nelly ft. Paul Wall',
+		album: 'Sweatsuit',
+		track: 'Grillz',
+		length: '4:32',
+		path: '/media/songs/Nelly-Grillz.mp3',
+		img: '/media/img/nelly.jpeg',
+	},
 ];
 
 const APP = {
 	player: null,
 	audio: null,
-	btnPlay: null,
+	playPauseBtn: null,
 	currentTrack: 0,
 	init: () => {
 		(APP.player = document.getElementById('player')),
 			(APP.audio = document.getElementById('audio')),
-			(APP.btnPlay = document.getElementById('btnPlay')),
+			(APP.playPauseBtn = document.getElementById('play_pause_btn')),
 			APP.addListeners();
 	},
 	addListeners: () => {
-		APP.btnPlay.addEventListener('click', APP.playTrack);
-		APP.btnStop.addEventListener('click', APP.stopTrack);
+		APP.playPauseBtn.addEventListener('click', APP.playTrack);
 	},
 	playTrack: ev => {
 		if (!APP.audio.paused) return; //already playing
-		APP.audio.src = SONGS[APP.currentTrack].src;
+		APP.audio.src = TRACKS[APP.currentTrack].src;
 		APP.audio.play();
 		APP.startAnimations();
 	},
